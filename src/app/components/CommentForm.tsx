bd8f1ef6795e42739b20495529431a5d656b8649
Rioba-Ian <riobafelix@gmail.com>
2024-01-26 01:16:34 +0300

    styling comment section for user in mobile

--- src/app/components/CommentForm.tsx
+++ src/app/components/CommentForm.tsx
@@ -17,8 +17,11 @@ type CommentFormProps = {
 export default function CommentForm({ user }: CommentFormProps) {
  return (
   <section className="w-full">
-   <form action="" className="grid grid-cols-12">
-    <div id="user-profile" className="">
+   <form
+    action=""
+    className="grid grid-col-1 grid-row-2 sm:grid-row-1 sm:grid-cols-12 -order-1"
+   >
+    <div id="user-profile" className="hidden sm:block">
      {/* user profile picture here */}
      <Image
       src={user.image ?? ""}
@@ -29,14 +32,30 @@ export default function CommentForm({ user }: CommentFormProps) {
      />
     </div>
 
-    <div id="form-box" className="col-span-10 flex items-start gap-4">
+    <div
+     id="form-box"
+     className="col-span-11 flex flex-col sm:flex-row items-start gap-4"
+    >
      <textarea
       rows={4}
+      placeholder="Add a comment..."
       className="py-2 px-4 w-full rounded-lg focus-visible:border-gray-500 focus-visible:ring-gray-500"
      ></textarea>
-     <FormSubmitButton className="py-2 px-4 rounded-lg bg-moderate-blue text-white ">
-      Send
-     </FormSubmitButton>
+     <div className="w-full flex flex-row items-center justify-between">
+      <div id="user-profile" className=" sm:hidden">
+       {/* user profile picture here */}
+       <Image
+        src={user.image ?? ""}
+        alt={user.name || "anonymous user"}
+        width={32}
+        height={32}
+        className="rounded-full block"
+       />
+      </div>
+      <FormSubmitButton className="py-2 px-4 rounded-lg bg-moderate-blue text-white ">
+       Send
+      </FormSubmitButton>
+     </div>
     </div>
    </form>
   </section>
