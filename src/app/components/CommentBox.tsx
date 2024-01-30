49ed76d12b2c24a537b5d06cd7c86f8a6ee91406
Rioba-Ian <riobafelix@gmail.com>
2024-01-30 23:44:35 +0300

    add reply button to each of the comments

--- src/app/components/CommentBox.tsx
+++ src/app/components/CommentBox.tsx
@@ -96,21 +96,34 @@ export default function CommentBox(props: CommentBoxProps) {
      </div>
 
      <div id="content-wrapper" className="flex flex-col justify-between">
-      <div id="content" className="flex gap-3 items-center">
-       <div id="profile">
-        <Image
-         src={props.image || PlaceHolderImage}
-         height={36}
-         width={36}
-         alt="some placeholder image"
-        />
+      <div
+       id="reply-content-info"
+       className="flex justify-between items-center"
+      >
+       <div id="content" className="flex gap-3 items-center">
+        <div id="profile">
+         <Image
+          src={props.image || PlaceHolderImage}
+          height={36}
+          width={36}
+          alt="some placeholder image"
+         />
+        </div>
+
+        <p className="font-semibold">{props.name}</p>
+        <span className="text-grayish-blue">
+         {props.modifiedAt && timeSince(props.modifiedAt)}
+        </span>
        </div>
 
-       <p className="font-semibold">{props.name}</p>
-       <span className="text-grayish-blue">
-        {props.modifiedAt && timeSince(props.modifiedAt)}
-       </span>
+       <div id="reply" className="hidden sm:block">
+        <button className="flex items-center space-x-2 ">
+         <Image height={16} width={16} alt="reply button" src={ReplyIcon} />
+         <span>Reply</span>
+        </button>
+       </div>
       </div>
+
       <div>
        <p className="text-grayish-blue mt-4">{props.content}</p>
       </div>
